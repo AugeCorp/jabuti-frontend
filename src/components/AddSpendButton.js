c0dd400233bc6ab0d2a4a70b0ddc1a955a51be93
nathaliebs <nathalie.brito@avante.com.vc>
2020-06-25 20:23:00 -0300

    Creating AddSpendButton component

---
+++ src/components/AddSpendButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button, Icon } from '@ui-kitten/components'
+
+const GradientButton = () => {
+  return (
+    <View>
+      <Button>
+      <Icon style={styles.icon} fill='black' name='plus-outline' />
+      </Button>
+    </View>
+  )
+}
+
+// const styles = StyleSheet.create({
+// })
+
+export default GradientButton
