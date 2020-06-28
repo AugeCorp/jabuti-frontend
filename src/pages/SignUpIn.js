15ab71ef2ac69d3384456a79bca9e9503cf61988
nathaliebs <nathalie.brito@avante.com.vc>
2020-06-28 20:00:32 -0300

    adding state to the login page inputs

--- src/pages/SignUpIn.js
+++ src/pages/SignUpIn.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { Input, Text } from '@ui-kitten/components';
 import { StyleSheet, View } from 'react-native';
 import GradientButton from '../components/GradientButton';
 
 const SignUpIn = () => {
-	const [value, setValue] = React.useState('');
-		return (
-			<View>
-				<Text style={styles.text} category='label'>fazer login:</Text>
-				<Input style={styles.emailInputField} placeholder='e-mail' value={value} onChangeText={nextValue => setValue(nextValue)} />
-				<Input style={styles.passInputField} placeholder='senha' value={value} onChangeText={nextValue => setValue(nextValue)} />
-				<GradientButton />
-			</View>
-  );
+	const [value, setValue] = useState('')
+	
+	return (
+		<View>
+			<Text style={styles.text} category='label'>fazer login:</Text>
+			<Input style={styles.inputField} placeholder='e-mail' value={value} onChangeText={nextValue => setValue(nextValue)} />
+			<Input style={styles.inputField} placeholder='senha' value={value} onChangeText={nextValue => setValue(nextValue)} />
+		</View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -21,18 +21,10 @@ const styles = StyleSheet.create({
 		marginTop: 250,
 		margin: 30,
 	},
-	emailInputField: {
+	inputField: {
 		margin: 30,
 		marginTop: 0,
 	},
-	passInputField: {
-		margin: 30,
-		marginTop: 0,
-	},
-	button: {
-		margin: 30,
-		marginTop: 10,
-	}
 })
 
 export default SignUpIn
