e9cf61095e52b078b9e36d2550d7654bc97a479f
nathaliebs <nathalie.brito@avante.com.vc>
2020-06-29 23:22:12 -0300

    adding conditional rendering to the components

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { StyleSheet, ScrollView } from 'react-native'
 import ExpensesList from '../components/ExpensesList'
-import AddExpenseModal from '../components/AddExpensesModal'
 
 const Expenses = () => {
   return (
