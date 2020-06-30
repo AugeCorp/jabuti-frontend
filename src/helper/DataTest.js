23dee2e03d4cd388ad4b1a791105c7379c587eb2
Arthur Bruno <arthur.abbrr@gmail.com>
2020-06-30 16:12:52 -0300

    test data file

---
+++ src/helper/DataTest.js
@@ -0,0 +1,98 @@
+export const expenses = [
+  {
+    id: 1,
+    description: 'Banana',
+    price: '2,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 2,
+    description: 'Café',
+    price: '1,50',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 3,
+    description: 'Maçã',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 4,
+    description: 'Manga',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 5,
+    description: 'Café',
+    price: '1,50',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 6,
+    description: 'Maçã',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 7,
+    description: 'Manga',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 8,
+    description: 'Café',
+    price: '1,50',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 9,
+    description: 'Maçã',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 10,
+    description: 'Manga',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 11,
+    description: 'Café',
+    price: '1,50',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 12,
+    description: 'Maçã',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 13,
+    description: 'Manga',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 14,
+    description: 'Café',
+    price: '1,50',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 15,
+    description: 'Maçã',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 16,
+    description: 'Manga',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 17,
+    description: 'Café',
+    price: '1,50',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 18,
+    description: 'Maçã',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  }, {
+    id: 19,
+    description: 'Manga',
+    price: '7,00',
+    icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+  },
+]
