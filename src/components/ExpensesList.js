641dca89357f73ba21895755bf0ff30738a922b8
Arthur Bruno <arthur.abbrr@gmail.com>
2020-06-30 15:53:59 -0300

    applying flex style to the expense page

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, ScrollView } from 'react-native'
 import { Card, Input, Text, Icon } from '@ui-kitten/components'
 import AddExpenseModal from './AddExpensesModal'
-import { colors } from '../helper/GlobalStyle'
+import { colors, text } from '../helper/GlobalStyle'
 
 const ExpensesList = () => {
   const [rows, setRows] = useState([])
@@ -28,6 +28,81 @@ const ExpensesList = () => {
       description: 'Manga',
       price: '7,00',
       icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 5,
+      description: 'Café',
+      price: '1,50',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 6,
+      description: 'Maça',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 7,
+      description: 'Manga',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 8,
+      description: 'Café',
+      price: '1,50',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 9,
+      description: 'Maça',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 10,
+      description: 'Manga',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 11,
+      description: 'Café',
+      price: '1,50',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 12,
+      description: 'Maça',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 13,
+      description: 'Manga',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 14,
+      description: 'Café',
+      price: '1,50',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 15,
+      description: 'Maça',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 16,
+      description: 'Manga',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 17,
+      description: 'Café',
+      price: '1,50',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 18,
+      description: 'Maça',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
+    }, {
+      id: 19,
+      description: 'Manga',
+      price: '7,00',
+      icon: <Icon style={styles.icon} fill={colors.primary} name='arrow-ios-downward-outline' />,
     },
   ]
 
@@ -43,18 +118,23 @@ const ExpensesList = () => {
   return (
     <View>
       <Card style={styles.card}>
+        <AddExpenseModal />
         <Input placeholder='Pesquisar por nome, data, valor...' onChangeText={value => handleTextInput(value)} />
         <Text style={styles.date} category="c2">24/06/2020</Text>
         <ScrollView> 
           {rows.map(row => (
-            <Card style={styles.listCard} key={row.id}>
+            <View style={styles.listCard} key={row.id}>
               {row.icon}
-              <Text category="h6">{row.description}</Text>
-              <Text category="h6">RS {row.price}</Text>
-            </Card>
+              <View style={styles.text}>
+                <Text style={styles.description}>{row.description}</Text>
+                <View style={styles.price}>
+                  <Text style={styles.symbol}>R$ </Text>
+                  <Text style={styles.number}>{row.price}</Text>
+                </View>
+              </View>
+            </View>
           ))}
         </ScrollView>
-        <AddExpenseModal />
       </Card>
     </View>
   )
@@ -71,15 +151,34 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   listCard: {
-    backgroundColor: 'white',
-    borderColor: 'white',
-    borderRadius: 15,
-    marginBottom: 8,
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 10,
+    flex: 1,
+    flexDirection: 'row',
   },
   icon: {
     width: 30,
     height: 30,
   },
+  text: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  description: text.light18,
+
+  price: {
+    flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    alignItems: 'flex-end',
+    right: 0,
+  },
+  symbol: text.light14,
+  number: text.light18,
 })
 
 export default ExpensesList
