369792d9bff0a80d6a48f12a4efb70b628815fb0
Arthur Bruno <arthur.abbrr@gmail.com>
2020-06-30 15:51:39 -0300

    including global text style

--- src/helper/GlobalStyle.js
+++ src/helper/GlobalStyle.js
@@ -3,4 +3,38 @@ export const colors = {
   secondary: '#175FC2',
   grey: '#6B6B6B',
   midGrey: '#9F9F9F',
+  white: '#FFFFFF'
+}
+
+export const text = {
+  light14: {
+    fontSize: 14,
+  },
+  light18: {
+    fontSize: 18,
+  },
+  light20: {
+    fontSize: 20,
+  },
+  light25: {
+    fontSize: 25,
+  },
+  medium12: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  medium14: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  medium18: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  regular18: {
+    fontSize: 18,
+  },
+  regular18: {
+    fontSize: 18,
+  }
 }
